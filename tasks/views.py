be5ac55a2562e8e97cee97b471e9d156895314b2
enridami <enri124589@gmail.com>
2025-10-19 19:16:03 -0300

    Add create_task

--- tasks/views.py
+++ tasks/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
-
 
 def signup(request):
     if request.method == 'GET':
@@ -35,15 +35,12 @@ def signup(request):
             "error": 'Password do not match'
         })
 
-
 def tasks(request):
     return render(request, 'tasks.html')
-
 
 def signout(request):
     logout(request)
     return redirect('home')
-
 
 def signin(request):
     if request.method == 'GET':
@@ -62,3 +59,20 @@ def signin(request):
             login(request, user)
             return redirect('tasks')
 
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': TaskForm
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+            'form': TaskForm,
+            'error': 'Please provide valide date'
+            })

f9b5ec1703fb2cff9893dd0fa88999f3c65fd9ba
Enrique Suica <enri1993@live.com>
2025-10-21 15:58:51 -0300

    Fix bootstrap home and create-task

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important'] 
-        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese un titulo'}),
+            'description': forms.Textarea(attrs={'class': 'form-control' , 'placeholder': 'Descripcion de la tarea..'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-text-input m-auto'}),
+        }
